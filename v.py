39f1c4eafa08b31d9363d891abdf273031dfa082
VIKAS RAWAT <66811679+vikasr20012001@users.noreply.github.com>
2020-09-27 00:37:46 +0530

    Create v.py

---
+++ v.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import cgi
+import subprocess as y
+print("content-type:text/html")
+print()
+
+
+form=cgi.FieldStorage()
+
+p=form.getvalue('a')
+
+output=y.getoutput("sudo " + cmd)
+
+print(output)
+
